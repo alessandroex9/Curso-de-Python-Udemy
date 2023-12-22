32ab448972f4ce230f4dcb8c2702d9c93c2ecba6
Alessandro Martins dos Santos <alessandromartinsdosantos@gmail.com>
2023-12-22 20:29:12 -0300

    21/12

--- Aulas/Aula-116.py
+++ Aulas/Aula-116.py
@@ -1,12 +1,14 @@
 """
 Closure e funções que retornam outras funções
 """
-def criar_saudacao(saudacao, nome):
-    def saudar():
+def criar_saudacao(saudacao):
+    def saudar(nome):
         return f'{saudacao}, {nome}!'
     return saudar 
 
-s1 = criar_saudacao('Bom dia', 'Luiz')
-s2 = criar_saudacao('Boa Noite', 'Luiz')
-print(s1())
-print(s2())
+falar_bom_dia = criar_saudacao('Bom dia')
+falar_boa_noite = criar_saudacao('Boa Noite')
+
+for nome in ['Alessandro', 'Maria', 'Marcos']:
+    print (falar_bom_dia(nome))
+    print (falar_boa_noite(nome))
